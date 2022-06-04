2567b7e3233e6b98606957bc20ed739e2b3bb2ec
herediasol <herediasolana@gmail.com>
2022-06-04 12:40:15 -0300

    Entrar

--- AppNQV/models.py
+++ AppNQV/models.py
@@ -1,3 +1,36 @@
+from random import choices
+from secrets import choice
 from django.db import models
+from django.forms import CharField
 
 # Create your models here.
+
+CATEGORIAS=(
+    ('accion', 'ACCION'),
+    ('drama','DRAMA'),
+    ('suspenso','SUSPENSO'),
+    ('terror','TERROR'),
+    ('documental','DOCUMENTAL'),
+    ('romatica','ROMANTICA'),
+)
+
+class Peliculas(models.Model):
+    nombre: models.CharField(max_length=40)
+    duracion: models.TimeField()
+    clasificacion: models.CharField(max_length=20, choices=CATEGORIAS, default='drama')
+    fechaDeEstreno: models.DateField(blank=True, null=True)
+    oscar: models.BooleanField(default=False)
+    ratingIMDB: models.IntegerField()
+    linkTrailer: models.URLField()
+
+class Actores(models.Model):
+    nombre:models.CharField(max_length=40)
+    apellido:models.CharField(max_length=40)
+    edad:models.IntegerField()
+    origen:models.CharField(max_length=40)
+    fechaDeNacimiento:models.DateField(blank=True, null=True)
+
+class Plataformas(models.Model):
+    cantidadUsuarios:models.IntegerField()
+    cantidadPeliculasDisponibles:models.IntegerField()
+    precioSuscripcion:models.DecimalField(max_digits=6, decimal_places=2)
