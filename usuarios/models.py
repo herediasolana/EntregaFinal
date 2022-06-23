fdda31ebda007f953298bb0b118dc8881d2faac2
herediasol <herediasolana@gmail.com>
2022-06-23 20:56:07 -0300

    ya se linkean usuarios con perfiles

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+def user_directory_path(instance, filename):
+    return 'imagen_perfil/{0}/{1}'.format(instance.user.id, filename)
+
 
 class Perfil_usuario(models.Model):
     usuario = models.OneToOneField(User, on_delete=models.CASCADE, related_name= 'perfil_usuarios')
@@ -8,8 +14,13 @@ class Perfil_usuario(models.Model):
     telefono= models.CharField(max_length=20, blank=True, null=True)
     email= models.EmailField(blank=True, null=True)
     pais= models.CharField(max_length=30, blank=True, null=True)
-    imagen_perfil= models.ImageField(upload_to='imagen_perfil', default='imagen_perfil/perfil_sin_foto.png')
+    imagen_perfil= models.ImageField(upload_to=user_directory_path, default='imagen_perfil/perfil_sin_foto.png')
 
     class Meta:
         verbose_name= 'usuario'
         verbose_name_plural= 'usuarios'
+
+@ receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs): #sender es el usuario, instance
+    if created:
+        Perfil_usuario.objects.create(usuario=instance)
